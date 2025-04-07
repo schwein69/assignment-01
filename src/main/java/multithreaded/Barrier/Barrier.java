31a88e3906e477759dd58d0ef2f37412b8a1b6fc
jiahao Guo <93580656+schwein69@users.noreply.github.com>
2025-04-07 21:37:26 +0200

    Update Barrier.java

--- src/main/java/multithreaded/Barrier/Barrier.java
+++ src/main/java/multithreaded/Barrier/Barrier.java
@@ -1,14 +1,5 @@
 package multithreaded.Barrier;
 
-/**
- * 
- * Barrier behaviour
- * 
- * N agents blocks until they *all* arrive to a common sync point ("barrier")
- * 
- */
 public interface Barrier {
-
 	void hitAndWaitAll() throws InterruptedException;
-
 }
